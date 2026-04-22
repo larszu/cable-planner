db6f286480feaeee9edf87c08fca9e7026f825eb
Lars Zumpe <sral89757@gmail.com>
2026-04-22 17:50:11 +0200

    build: fix self-referencing dep causing OOM; add portable+nsis targets; output to release/

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -4,11 +4,21 @@ export default {
   files: ['dist/**/*', 'package.json'],
   directories: {
     buildResources: 'build',
+    output: 'release',
   },
   mac: {
     target: ['dmg'],
   },
   win: {
-    target: ['nsis'],
+    target: ['nsis', 'portable'],
+    artifactName: '${productName}-${version}-${arch}.${ext}',
+  },
+  nsis: {
+    oneClick: false,
+    allowToChangeInstallationDirectory: true,
+    perMachine: false,
+  },
+  portable: {
+    artifactName: '${productName}-${version}-portable.${ext}',
   },
 }
